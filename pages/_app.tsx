fdb17ba4bcf41d45b4447449b7508a049aac24b7
Octavian Dima <hello@wopss.net>
2021-06-27 02:11:37 +0300

    Add tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app"
 
+import '../styles/_app.css';
+
 function App({ Component, pageProps }: AppProps): JSX.Element {
     return <Component {...pageProps} />;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mode: "jit",
+    darkMode: "class",
+    purge: [
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}"
+    ]
+  }

751cbbe30aac0e52377d88aad36038d56aed5ad9
Octavian Dima <hello@wopss.net>
2021-07-09 23:26:52 +0300

    Add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+
+const routes: Routes = [{ path: "**", component: PageNotFoundComponent }];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 @NgModule({
     bootstrap: [AppComponent],
     imports: [BrowserModule, AppRoutingModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, PageNotFoundComponent],
     providers: []
 })
 export class AppModule {}

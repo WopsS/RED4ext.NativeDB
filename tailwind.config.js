f81146b3386fcca3a2a8713b291eaf616ff4529f
Octavian Dima <hello@wopss.net>
2021-07-09 22:24:01 +0300

    Add Fira Code font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     mode: "jit",
     darkMode: false,
-    purge: ["./src/**/*.{html,ts}"]
+    purge: ["./src/**/*.{html,ts}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                mono: [
+                    "'Fira Code VF'",
+                    "'Fira Code'",
+                    ...defaultTheme.fontFamily.mono
+                ]
+            }
+        }
+    }
 };

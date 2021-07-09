750553ef7ca4f9736c77498b3d653b47df699184
Octavian Dima <hello@wopss.net>
2021-07-09 23:49:27 +0300

    Fix build error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
 
 @NgModule({

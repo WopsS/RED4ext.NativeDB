047df9239595f16f0f96ba85ef18ce1192b44dd2
Octavian Dima <3403191+WopsS@users.noreply.github.com>
2022-09-24 22:37:47 +0200

    Move the fonts and font smoothing to SCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,4 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-    content: ["./src/**/*.{html,ts}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                mono: [
-                    "'Fira Code VF'",
-                    "'Fira Code'",
-                    ...defaultTheme.fontFamily.mono
-                ]
-            }
-        }
-    }
+    content: ["./src/**/*.{html,ts}"]
 };

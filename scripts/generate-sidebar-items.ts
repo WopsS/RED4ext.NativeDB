44719ec73edc4f165abfb2748a3922ca2dc77025
Octavian Dima <3403191+WopsS@users.noreply.github.com>
2022-09-24 22:53:00 +0200

    Improve generation of the sidebar items

--- scripts/generate-sidebar-items.ts
+++ scripts/generate-sidebar-items.ts
@@ -1,32 +1,34 @@
 import * as fs from "fs-extra";
 import * as path from "node:path";
 
-import { collectNatives } from "./utils/collect-natives";
+enum TypeDirectory {
+    Bitfields = "bitfields",
+    Classes = "classes",
+    Enums = "enums"
+}
 
-async function generate(inputDir: string, outputFile: string): Promise<void> {
+export async function collectNatives(dir: string): Promise<string[]> {
+    const files = await fs.readdir(dir);
+    return files
+        .map((f) => path.basename(f, path.extname(f))) // Remove the extension.
+        .sort((lhs, rhs) => lhs.localeCompare(rhs)); // Sort alphabetically.
+}
+
+async function generate(type: TypeDirectory): Promise<void> {
+    const cwd = process.cwd();
+    const assetsDir = path.join(cwd, "src", "assets");
+    const nativesDir = path.join(assetsDir, "natives");
+    const inputDir = path.join(nativesDir, type);
+    const outputFile = path.join(nativesDir, `${type}.json`);
+
     const files = await collectNatives(inputDir);
     await fs.writeJson(outputFile, files /* , { spaces: 2 } */);
 }
 
 async function main(): Promise<void> {
-    const cwd = process.cwd();
-    const assetsDir = path.join(cwd, "src", "assets");
-    const nativesDir = path.join(assetsDir, "natives");
-
-    await generate(
-        path.join(nativesDir, "bitfields"),
-        path.join(nativesDir, "bitfields.json")
-    );
-
-    await generate(
-        path.join(nativesDir, "classes"),
-        path.join(nativesDir, "classes.json")
-    );
-
-    await generate(
-        path.join(nativesDir, "enums"),
-        path.join(nativesDir, "enums.json")
-    );
+    await generate(TypeDirectory.Bitfields);
+    await generate(TypeDirectory.Classes);
+    await generate(TypeDirectory.Enums);
 }
 
 main().catch(console.error);
--- scripts/utils/collect-natives.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs-extra";
-import * as path from "node:path";
-
-export async function collectNatives(dir: string): Promise<string[]> {
-    const files = await fs.readdir(dir);
-    return files
-        .map((f) => path.basename(f, path.extname(f))) // Remove extension.
-        .sort((lhs, rhs) => lhs.localeCompare(rhs)); // Sort alphabetically.
-}
